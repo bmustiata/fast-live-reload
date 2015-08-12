441ccc47b94749ba135e202676e70aed49fc4df9
Bogdan Mustiata <bogdan.mustiata@gmail.com>
2015-08-12 14:43:26 +0200

    Don't activate live-reloading if it's active in the parent iframe.

--- iframe/fast-live-reload/js/client-reload.js
+++ iframe/fast-live-reload/js/client-reload.js
@@ -1,5 +1,12 @@
 (function() { // don't pollute the environment.
 
+// mark it anyway, so if we have nested iframes,
+// we'll still have it set correctly.
+window._ciplogic_fast_live_reload = true;
+
+if (parent != window && parent._ciplogic_fast_live_reload) {
+	return; // bail out if loaded in the parent iframe.
+}
 
 /**
  * AjaxCall - A class that performs an AJAX call, and invokes the given callbacks.
--- src/client/_wrap-before.js
+++ src/client/_wrap-before.js
@@ -1,2 +1,9 @@
 (function() { // don't pollute the environment.
 
+// mark it anyway, so if we have nested iframes,
+// we'll still have it set correctly.
+window._ciplogic_fast_live_reload = true;
+
+if (parent != window && parent._ciplogic_fast_live_reload) {
+	return; // bail out if loaded in the parent iframe.
+}
