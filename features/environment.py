a662f344c7364b00bd011fc3bb887e7b6e29bacc
Bogdan Mustiata <bogdan.mustiata@gmail.com>
2017-05-10 16:09:01 +0200

    Fixed the tests not starting.

--- features/environment.py
+++ features/environment.py
@@ -1,5 +1,6 @@
 import shutil
 import tempfile
+import os.path
 
 from germanium.static import get_germanium, close_browser
 
@@ -7,7 +8,7 @@ from germanium.static import get_germanium, close_browser
 def before_scenario(context, scenario):
     context.test_data_folder = tempfile.gettempdir() + '/flr-test'
     shutil.rmtree(context.test_data_folder, ignore_errors=True)
-    shutil.copytree("features/test-data", context.test_data_folder)
+    shutil.copytree(os.path.realpath("features/test-data/"), context.test_data_folder)
 
 
 def after_scenario(context, scenario):
